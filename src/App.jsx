eb752699141e905ff7cfbeb56e529a6c859db389
Glen Palmer <glen.palmer@hotmail.co.uk>
2025-05-27 09:46:06 +0100

    start the project section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AboutMeMain from "./assets/components/sectionAboutMe/AboutMeMain";
 import HeroMain from "./assets/components/sectionHero/HeroMain";
 import NavbarMain from "./assets/components/sectionNavbar/NavbarMain";
 import ExperienceMain from "./assets/components/sectionExperience/ExperienceMain";
+import ProjectsMain from "./assets/components/sectionProjects/ProjectsMain";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <HeroMain />
         <AboutMeMain />
         <ExperienceMain />
+        <ProjectsMain />
       </div>
     </>
   );
