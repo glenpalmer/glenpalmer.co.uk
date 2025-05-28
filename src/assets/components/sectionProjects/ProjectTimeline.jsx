63caf817cd8c64b5b04871b3c2c1e03510149380
Glen Palmer <glen.palmer@hotmail.co.uk>
2025-05-28 18:12:09 +0100

    Layout of the Project Time Line

--- src/assets/components/sectionProjects/ProjectTimeline.jsx
+++ src/assets/components/sectionProjects/ProjectTimeline.jsx
@@ -1,30 +1,94 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ProjectData from "./ProjectData.json";
+import { CiGrid2V } from "react-icons/ci";
 
 const ProjectDataSet = ProjectData.Projects;
 
+const Circle = () => {
+  return <div className="rounded-full w-4 h-4 bg-blue-500 mx-auto"></div>;
+};
+
+const Pillar = () => {
+  return (
+    <div className="rounded-t-full rounded-b-full w-2 h-full bg-blue-500 mx-auto"></div>
+  );
+};
+
+const EventCard = ({ client, role }) => {
+  return (
+    <div className="flex flex-col gap-y-2 border shadow-md rounded-xl p-4">
+      <div className="text-blue-800 font-bold text-lg border-b">{client}</div>
+      <div className="text-sm text-grey-700">{role}</div>
+    </div>
+  );
+};
+
 const ProjectTimeLine = () => {
   return (
-    <div>
-      <div className="min-h-screen flex justify-center pt-20">
-        <div class="w-80">
-          <h2 className="text-xl text-gray-700 mb-7">Recent Updates</h2>
-          <ul>
-            <li className="relative flex gap-6 items-baseline">
-              <div className="before:absolute before:left-[6px] before:h-full before:w-[3px] before:bg-gray-400">
-                <div className="h-[15px] w-[15px] rounded-full bg-gray-400"></div>
-              </div>
-              <div className="text-sm text-gray-600">
-                <p>27.08.2022</p>
-                <p className="mt-2 pb-6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-                  possimus ducimus quos velit ratione consequatur eius veniam
-                  neque ea quae, suscipit cupiditate fuga saepe aliquam beatae,
-                  fugit dolor nisi sed.
-                </p>
-              </div>
-            </li>
-          </ul>
+    <div className="border rounded-3xl w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
+      <div className="border flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16 mt-4">
+        <div className="w-full max-w-3xl mx-auto">
+          {/*<!-- Vertical Timeline #1 -->*/}
+          <div className="-my-6">
+            {ProjectDataSet.map((project) => {
+              return (
+                <div>
+                  {/* Left side of the time line */}
+                  {project.position === "left" ? (
+                    <div className="flex justify-start">
+                      <div className="w-[350px] text-right">
+                        <div className="flex flex-col items-end">
+                          <div className="relative uppercase w-[150px] border text-center py-1 mb-2 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
+                            {project.date}
+                          </div>
+                          <div className="font-caveat font-medium text-3xl text-indigo-500 pb-2">
+                            {project.client}
+                          </div>
+                          <div className="text-lg font-bold text-slate-500">
+                            {project.role}
+                          </div>
+                          <div className="text-md text-slate-500">
+                            {project.primeSi} - {project.type}
+                          </div>
+                          <div className="text-sm text-">
+                            <p>{project.summary}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                  {/* Right side of the time line */}
+                  {project.position === "right" ? (
+                    <div className="flex justify-end">
+                      <div className="w-[350px] text-left">
+                        <div className="flex flex-col items-start">
+                          <div className="relative uppercase w-[150px] border text-center py-1 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
+                            {project.date}
+                          </div>
+                          <div className="font-caveat font-medium text-3xl text-indigo-500 pb-2">
+                            {project.client}
+                          </div>
+                          <div className="text-lg font-bold text-slate-500">
+                            {project.role}
+                          </div>
+                          <div className="text-md text-slate-500">
+                            {project.primeSi} - {project.type}
+                          </div>
+                          <div className="text-sm text-">
+                            <p>{project.summary}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/assets/components/sectionProjects/ProjectTimelineSample.jsx
+++ src/assets/components/sectionProjects/ProjectTimelineSample.jsx
@@ -6,78 +6,105 @@ const ProjectDataSet = ProjectData.Projects;
 const ProjectTimelineSample = () => {
   return (
     <div className="border rounded-3xl w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
-      <div className="flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16">
-        <div className="w-full max-w-3xl mx-auto">
+      <div className="border flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16">
+        <div className="border w-full max-w-3xl mx-auto">
           {/*<!-- Vertical Timeline #1 -->*/}
-          <div className="-my-6">
+          <div className="border -my-6">
             {/*<!-- Item #1 -->*/}
-            <div className="relative pl-8 sm:pl-32 py-6 group">
-              {/*<!-- Purple label -->*/}
-              {ProjectDataSet.map((project) => (
-                <div key={project.id}>
-                  <div className="font-caveat font-medium text-3xl text-indigo-500 mb-1 sm:mb-0">
+            {ProjectDataSet.map((project) => (
+              <div className="border relative pl-8 sm:pl-32 py-6 group">
+                {/*<!-- Purple label -->*/}
+                <div key={project.id} className="border">
+                  <div className="border font-caveat font-medium text-3xl text-indigo-500 mb-1 sm:mb-0">
                     {project.client}
                   </div>
                   {/*<!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->*/}
-                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                    <time className="sm:absolute -left-16 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-35 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
+                  <div className="border flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <time className="border sm:absolute -left-16 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-35 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
                       {project.date}
                     </time>
-                    <div className="text-xl font-bold text-slate-500">
+                    <div className="border text-xl font-bold text-slate-500">
                       {project.role}
                     </div>
                   </div>
 
                   {/*<!-- Content -->*/}
-                  <div className="text-black">{project.summary}</div>
-                </div>
-              ))}
-            </div>
-
-            {/*<!-- Item #2 -->*/}
-            <div className="relative pl-8 sm:pl-32 py-6 group">
-              {/*<!-- Purple label -->*/}
-              <div className="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">
-                The milestone
-              </div>
-              {/*<!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->*/}
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
-                  May, 2021
-                </time>
-                <div className="text-xl font-bold text-slate-900">
-                  Reached 5K customers
+                  <div className="border text-black pb-12">
+                    {project.summary}
+                  </div>
                 </div>
               </div>
-              {/*<!-- Content -->*/}
-              <div className="text-slate-500">
-                Pretium lectus quam id leo. Urna et pharetra pharetra massa
-                massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit
-                cursus risus.
-              </div>
-            </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/*<!-- New Version Attempt -->*/}
+      <div className="border flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16 mt-4">
+        <div className="w-full max-w-3xl mx-auto">
+          {/*<!-- Vertical Timeline #1 -->*/}
+          <div className="-my-6">
+            {ProjectDataSet.map((project) => {
+              return (
+                <div>
+                  {/* Left side of the time line */}
+                  {project.position === "left" ? (
+                    <div className="flex justify-start">
+                      <div className="w-[350px] text-right">
+                        <div className="flex flex-col items-end">
+                          <div className="relative uppercase w-[150px] border text-center py-1 mb-2 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
+                            {project.date}
+                          </div>
+                          <div className="font-caveat font-medium text-3xl text-indigo-500 pb-2">
+                            {project.client}
+                          </div>
+                          <div className="text-lg font-bold text-slate-500">
+                            {project.role}
+                          </div>
+                          <div className="text-md text-slate-500">
+                            {project.primeSi} - {project.type}
+                          </div>
+                          <div className="text-sm text-">
+                            <p>{project.summary}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                  {/* Right side of the time line */}
+                  {project.position === "right" ? (
+                    <div className="flex justify-end">
+                      <div className="w-[350px] text-left">
+                        <div className="flex flex-col items-start">
+                          <div className="relative uppercase w-[150px] border text-center py-1 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
+                            {project.date}
+                          </div>
+                          <div className="font-caveat font-medium text-3xl text-indigo-500 pb-2">
+                            {project.client}
+                          </div>
+                          <div className="text-lg font-bold text-slate-500">
+                            {project.role}
+                          </div>
+                          <div className="text-md text-slate-500">
+                            {project.primeSi} - {project.type}
+                          </div>
+                          <div className="text-sm text-">
+                            <p>{project.summary}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
     </div>
-    /*    <div className="border flex rounded-3xl">
-      <ul className="mx-auto">
-        {ProjectsDataSet.map((project) => (
-          <div className="mt-4">
-            <li key={project.id} className="flex gap-12">
-              <time className="left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-40 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
-                {project.date}
-              </time>
-              <div>
-                <h1>{project.client}</h1>
-                <h2>{project.role}</h2>
-                <p>{project.summary}</p>
-              </div>
-            </li>
-          </div>
-        ))}
-      </ul>
-    </div>*/
   );
 };
 
