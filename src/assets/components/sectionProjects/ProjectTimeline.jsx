4764c1666526fe6c42366bee1de1067e369abce7
Glen Palmer <glen.palmer@hotmail.co.uk>
2025-05-29 17:56:08 +0100

    Update ProjectTimeline.jsx

--- src/assets/components/sectionProjects/ProjectTimeline.jsx
+++ src/assets/components/sectionProjects/ProjectTimeline.jsx
@@ -10,16 +10,16 @@ const ProjectTimeLine = () => {
       <div className="flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16 mt-4">
         <div className="w-full max-w-3xl mx-auto">
           {/*<!-- Vertical Timeline #1 -->*/}
-          <div className="border border-green-500-my-6">
+          <div className="-my-6">
             {ProjectDataSet.map((project) => {
               return (
                 <div key={project.id}>
                   {/* Left side of the time line */}
                   {project.position === "left" ? (
-                    <div className="border border-red-500 flex m-2">
-                      <div className="border border-yellow-500 w-1/2">
-                        <div className="border flex flex-col items-end mt-1">
-                          <div className="font-caveat font-medium text-3xl text-indigo-500 pb-2">
+                    <div className="flex my-12">
+                      <div className="w-1/2 mr-4">
+                        <div className="flex flex-col items-end">
+                          <div className="font-caveat font-medium text-3xl text-indigo-500">
                             {project.client}
                           </div>
                           <div className="text-lg font-bold text-slate-500">
@@ -30,9 +30,9 @@ const ProjectTimeLine = () => {
                           </div>
                         </div>
                       </div>
-                      <div className="border border-indigo-500 w-1/2">
-                        <div className="border flex mt-1 justify-start">
-                          <div className="uppercase w-[150px] border text-center py-1 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
+                      <div className="w-1/2 ml-4">
+                        <div className="flex items-start">
+                          <div className="uppercase w-[150px] border text-center py-1 translate-y-1 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
                             {project.date}
                           </div>
                         </div>
@@ -43,17 +43,17 @@ const ProjectTimeLine = () => {
                   )}
                   {/* Right side of the time line */}
                   {project.position === "right" ? (
-                    <div className="border border-red-500 flex m-2">
-                      <div className="border border-indigo-500 w-1/2">
-                        <div className="border flex mt-1 justify-end">
-                          <div className="uppercase w-[150px] border text-center py-1 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
+                    <div className="flex my-12">
+                      <div className="w-1/2 mr-4">
+                        <div className="flex justify-end">
+                          <div className="uppercase w-[150px] border text-center py-1 translate-y-1 rounded-full text-xs font-semibold text-emerald-600 bg-emerald-100">
                             {project.date}
                           </div>
                         </div>
                       </div>
-                      <div className="border border-yellow-500 w-1/2">
-                        <div className="border flex flex-col items-start mt-1">
-                          <div className="font-caveat font-medium text-3xl text-indigo-500 pb-2">
+                      <div className="w-1/2 ml-4">
+                        <div className="flex flex-col items-start">
+                          <div className="font-caveat font-medium text-3xl text-indigo-500">
                             {project.client}
                           </div>
                           <div className="text-lg font-bold text-slate-500">
