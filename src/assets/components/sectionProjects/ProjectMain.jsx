a17bd0edee7618d05fcdef5f64914d49084bc481
Glen Palmer <glen.palmer@hotmail.co.uk>
2025-05-29 19:47:22 +0100

    Final touches to Project Timeline Region

--- src/assets/components/sectionProjects/ProjectMain.jsx
+++ src/assets/components/sectionProjects/ProjectMain.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ProjectTimelineSample from "./ProjectTimelineSample";
 import ProjectHeader from "./ProjectHeader";
 import ProjectTimeline from "./ProjectTimeline";
 
@@ -7,7 +6,6 @@ const ProjectMain = () => {
   return (
     <div className="max-w-[1180px] mx-auto pb-60 justify-center">
       <ProjectHeader />
-      <ProjectTimelineSample />
       <ProjectTimeline />
     </div>
   );
