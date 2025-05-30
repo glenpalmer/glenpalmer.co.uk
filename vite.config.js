ad00f4ff1461570967f46ae084b3972496708145
Glen Palmer <glen.palmer@hotmail.co.uk>
2025-05-30 17:06:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), (base: "/glenpalmer.co.uk")],
+  plugins: [react(), tailwindcss()],
+  base: "/glenpalmer.co.uk/",
 });
