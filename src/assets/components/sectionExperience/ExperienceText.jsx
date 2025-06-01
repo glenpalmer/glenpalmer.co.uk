cb5172b6841b53a836663e7d485df890f8699bc2
Glen Palmer <glen.palmer@hotmail.co.uk>
2025-06-01 09:11:41 +0100

    Update to experience
    Need to move this to an array instead of building every experience element, could also do the certificates

--- src/assets/components/sectionExperience/ExperienceText.jsx
+++ src/assets/components/sectionExperience/ExperienceText.jsx
@@ -7,7 +7,7 @@ const ExperienceText = () => {
   return (
     <div className="flex mt-2 md:mt-8">
       <div className="grow flex flex-col mx-4 md:flex-row md:justify-around md:gap-24">
-        <div className="border border-black rounded-3xl text-center pt-2 my-2 md:w-2/5">
+        <div className="border border-black rounded-3xl text-center pt-4 my-2 md:w-2/5">
           <h1 className="text-xl font-bold">Oracle Certifications</h1>
           <div className="flex flex-row md:flex-col md:items-center">
             <div className="">
@@ -32,45 +32,55 @@ const ExperienceText = () => {
             <table className="w-full">
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">Oracle Cloud Payroll</td>
+                <td className="text-left md:text-2xl">Oracle Cloud Payroll</td>
               </tr>
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">Oracle Cloud Core Human Resources</td>
+                <td className="text-left md:text-2xl">
+                  Oracle Cloud Core Human Resources
+                </td>
               </tr>
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">Oracle Cloud Absence Management</td>
+                <td className="text-left md:text-2xl">
+                  Oracle Cloud Absence Management
+                </td>
               </tr>
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">Oracle Cloud Time & Labor</td>
+                <td className="text-left md:text-2xl">
+                  Oracle Cloud Time & Labor
+                </td>
               </tr>
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">Oracle Cloud Fast Formula</td>
+                <td className="text-left md:text-2xl">
+                  Oracle Cloud Fast Formula
+                </td>
               </tr>
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">Oracle HCM Data Load (HDL)</td>
+                <td className="text-left md:text-2xl">
+                  Oracle HCM Data Load (HDL)
+                </td>
               </tr>
               <tr>
                 <td className="py-2">
-                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500" />
+                  <TbRosetteDiscountCheckFilled className="text-xl text-blue-500 md:text-3xl" />
                 </td>
-                <td className="text-left">
+                <td className="text-left md:text-2xl">
                   Oracle Transactional Business Intelligence (OTBI)
                 </td>
               </tr>
